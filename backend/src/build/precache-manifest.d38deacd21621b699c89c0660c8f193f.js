eaff194cf7d0a9817ef77e5c942edfc100402f49
PRAMOD A JINGADE <avj2352@gmail.com>
2021-01-28 21:30:57 +0800

    backend API ready for Jmeter-testing

---
+++ backend/src/build/precache-manifest.d38deacd21621b699c89c0660c8f193f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3f75cd7d4877c40169784edff9c30459",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d81dd4676bdc047b0eb3",
+    "url": "/static/css/main.e17e5426.chunk.css"
+  },
+  {
+    "revision": "747122f43a049e971aaa",
+    "url": "/static/js/2.c1a6c5fb.chunk.js"
+  },
+  {
+    "revision": "bc2e6be796bb512c81264291fcc91421",
+    "url": "/static/js/2.c1a6c5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81dd4676bdc047b0eb3",
+    "url": "/static/js/main.f51577c9.chunk.js"
+  },
+  {
+    "revision": "2747b914e9a60db2276f",
+    "url": "/static/js/runtime-main.c8a21426.js"
+  },
+  {
+    "revision": "a8fc048e7729ab3c762e2ea87c763378",
+    "url": "/static/media/broke.a8fc048e.png"
+  },
+  {
+    "revision": "75562895c65b45eab9ca68a45f830863",
+    "url": "/static/media/logo.75562895.png"
+  }
+]);
